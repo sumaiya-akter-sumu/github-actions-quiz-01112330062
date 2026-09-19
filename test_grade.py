4e05ab7a2646d0ec657d2119a0ba8cf222751974
sumaiya-akter-sumu <sakter2330062@bscse.uiu.ac.bd>
2026-09-19 13:22:00 +0600

    feat(quiz): add grade module and tests

---
+++ test_grade.py
@@ -0,0 +1,23 @@
+
+
+def test_a_grade():
+
+    assert get_grade(85) == "A"
+
+
+
+def test_b_grade():
+
+    assert get_grade(72) == "B"
+
+
+
+def test_c_grade():
+
+    assert get_grade(65) == "C"
+
+
+
+def test_f_grade():
+
+    assert get_grade(30) == "F"
