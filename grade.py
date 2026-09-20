e73b683be7e16d5c30fce6c4d966a7e6ee369a4e
sumaiya-akter-sumu <sakter2330062@bscse.uiu.ac.bd>
2026-09-20 13:31:42 +0600

    feat(quiz): add grade module and tests

--- grade.py
+++ grade.py
@@ -1,12 +1,9 @@
-
-def get_grade(marks):
-
-    if marks >= 80: return "A"
-
-    if marks >= 70: return "B"
-
-    if marks >= 60: return "C"
-
-    if marks >= 50: return "D"
-
-    return "F"
+def get_grade(mark):
+    if mark >= 80:
+        return "A"
+    elif mark >= 70:
+        return "B"
+    elif mark >= 60:
+        return "C"
+    else:
+        return "F"
